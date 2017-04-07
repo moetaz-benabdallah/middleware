5b2fa845259480ef86cf5d0afc6960f87362fcd6
MoatezBen Abdallah <moatez.abdallah@projetas.com.br>
2017-04-07 12:04:50 -0300

    some changes

--- src/main/java/com/efacil/web/crm/PersonCrmEndpointController.java
+++ src/main/java/com/efacil/web/crm/PersonCrmEndpointController.java
@@ -13,35 +13,32 @@ import com.efacil.domain.Person;
 import com.efacil.service.PersonService;
 
 @Controller
-@RequestMapping(value = "/crm/people")
+@RequestMapping(value = "/crm")
+@ResponseBody
 public class PersonCrmEndpointController {
 
 	@Autowired
 	PersonService personService;
 	
-	@RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-	@ResponseBody
-	Person personCreate(@RequestHeader(value = "Authorization", required=true) String credentials ,
+	@RequestMapping(value = "/people", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	Person create(@RequestHeader(value = "Authorization", required=true) String credentials ,
 			@RequestBody Person person){
 		return personService.create(credentials, person);
 	}
 	
-	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, consumes = "application/json")
-	@ResponseBody
+	@RequestMapping(value = "/people/{id}", method = RequestMethod.PUT, consumes = "application/json")
 	void update(@RequestHeader(value = "Authorization", required=true) String credentials ,
 			@RequestBody Person person, @PathVariable("id") Long id) {
 		personService.update(credentials, person, id);
 	}
 	
-	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	Person read(@RequestHeader(value = "Authorization", required=true) String credentials, @PathVariable("id") Long id){
+	@RequestMapping(value = "/people/{id}", method = RequestMethod.GET, produces = "application/json")
+	Person getOneById(@RequestHeader(value = "Authorization", required=true) String credentials, @PathVariable("id") Long id){
 		return personService.read(credentials, id);
 		
 	}
 	
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
-	@ResponseBody
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
 	void delete(@RequestHeader(value = "Authorization", required=true) String credentials, @PathVariable("id") Long id){
 		personService.destroy(credentials, id);
 		
--- src/main/java/com/efacil/web/legacy/PersonLegacyEndpointController.java
+++ src/main/java/com/efacil/web/legacy/PersonLegacyEndpointController.java
@@ -14,45 +14,40 @@ import com.efacil.domain.Person;
 import com.efacil.service.PeopleService;
 
 @Controller
-@RequestMapping(value = "/legacy/people")
+@RequestMapping(value = "/legacy")
+@ResponseBody
 public class PersonLegacyEndpointController {
 	
 	@Autowired
 	PeopleService peopleService;
 	
-	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	@ResponseBody
+	@RequestMapping(value = "/people", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	Person create(@RequestBody Person person) {
 		return peopleService.create(person);
 	}
 	
-	@RequestMapping(value = "/get", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	List<Person> getPeople() {
+	@RequestMapping(value = "/people", method = RequestMethod.GET, produces = "application/json")
+	List<Person> getAll() {
 		return peopleService.getAll();
 	}
 	
-	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	Person getById(@PathVariable("id") Long id) {
+	@RequestMapping(value = "/people/{id}", method = RequestMethod.GET, produces = "application/json")
+	Person getOneById(@PathVariable("id") Long id) {
 		return peopleService.getOne(id);
 	}
 	
-	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-	@ResponseBody
-	void peopleDestroy(@PathVariable("id") Long id) {
+	@RequestMapping(value = "/people/{id}", method = RequestMethod.DELETE)
+	void delete(@PathVariable("id") Long id) {
 		peopleService.destroy(id);
 	}
 	
-	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, consumes = "application/json")
-	@ResponseBody
-	void peopleUpdate(@RequestBody Person person, @PathVariable("id") Long id) {
+	@RequestMapping(value = "/people/{id}", method = RequestMethod.PUT, consumes = "application/json")
+	void update(@RequestBody Person person, @PathVariable("id") Long id) {
 		peopleService.update(person, id);
 	}
 	
-	@RequestMapping(value = "/deleteAll", method = RequestMethod.GET)
-	@ResponseBody
-	void destroyAll() {
+	@RequestMapping(value = "/people", method = RequestMethod.DELETE)
+	void deleteAll() {
 		peopleService.destroyAll();
 	}
 	
